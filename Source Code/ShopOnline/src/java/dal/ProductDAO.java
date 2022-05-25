a76d9e89b378d846d85f634f404eb7818af3cb99
Hoang Viet <hoangnvliu@gmail.com>
2022-05-25 19:50:45 +0700

    update product

--- Source Code/ShopOnline/src/java/dal/ProductDAO.java
+++ Source Code/ShopOnline/src/java/dal/ProductDAO.java
@@ -5,10 +5,109 @@
  */
 package dal;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Product;
+import java.util.List;
 /**
  *
  * @author Administrator
  */
 public class ProductDAO {
+    DBConnect mysqlConnect = new DBConnect();
+
+    public List<Product> listAll() {
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM `products`";
+        try {
+            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getInt("product_id"));
+                p.setName(rs.getString("product_name"));
+                p.setPrice(rs.getFloat("unit_price"));
+                p.setDescription(rs.getString("description"));
+                p.setImg(rs.getString("url"));
+                products.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            mysqlConnect.disconnect();
+        }
+        return products;
+    }
+
+    public List<Product> listAll(String sort) {
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM `products` ORDER BY `products`.`unit_price` " + sort;
+        try {
+            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getInt("product_id"));
+                p.setName(rs.getString("product_name"));
+                p.setPrice(rs.getFloat("unit_price"));
+                p.setDescription(rs.getString("description"));
+                p.setImg(rs.getString("url"));
+                products.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            mysqlConnect.disconnect();
+        }
+        return products;
+    }
+
+    public List<Product> listTop(int quantity) {
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM `products` LIMIT ?";
+        try {
+            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+            statement.setInt(1, quantity);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getInt("product_id"));
+                p.setName(rs.getString("product_name"));
+                p.setPrice(rs.getFloat("unit_price"));
+                p.setImg(rs.getString("url"));
+                products.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            mysqlConnect.disconnect();
+        }
+        return products;
+    }
     
+    
+    public List<Product> listTopNew(int quantity) {
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM `products` ORDER BY product_id DESC LIMIT ?";
+        try {
+            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+            statement.setInt(1, quantity);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getInt("product_id"));
+                p.setName(rs.getString("product_name"));
+                p.setPrice(rs.getFloat("unit_price"));
+                p.setImg(rs.getString("url"));
+                products.add(p);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        } finally {
+            mysqlConnect.disconnect();
+        }
+        return products;
+    }
 }
--- Source Code/ShopOnline/src/java/model/Product.java
+++ Source Code/ShopOnline/src/java/model/Product.java
@@ -10,5 +10,130 @@ package model;
  * @author Administrator
  */
 public class Product {
-    
+    private int id;
+    private String name;
+    private int categoryid;
+    private float price;
+    private float salePrice;
+    private int stock;
+    private String brief_information;
+    private String description;
+    private String img;
+    private String sortdesc;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, float price, int stock, String description, String img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+        this.img = img;
+    }
+
+    public Product(int id, String name, float price, int stock, String description, String img, String sortdesc) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+        this.img = img;
+        this.sortdesc = sortdesc;
+    }
+    public Product(int id, String name, float price, float salePrice, int stock, String brief_information, String description, String img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.salePrice = salePrice;
+        this.stock = stock;
+        this.brief_information = brief_information;
+        this.description = description;
+        this.img = img;
+    }
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getBrief_information() {
+        return brief_information;
+    }
+
+    public void setBrief_information(String brief_information) {
+        this.brief_information = brief_information;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(int categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getSortdesc() {
+        return sortdesc;
+    }
+
+    public void setSortdesc(String sortdesc) {
+        this.sortdesc = sortdesc;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", categoryid=" + categoryid + ", price=" + price + ", salePrice=" + salePrice + ", stock=" + stock + ", brief_information=" + brief_information + ", description=" + description + ", img=" + img + ", sortdesc=" + sortdesc + '}';
+    }
 }
