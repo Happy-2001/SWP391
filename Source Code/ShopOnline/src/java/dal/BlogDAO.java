b4bc3432ff95f5409b329c317e5e4dd7fda6a430
ManhHung01 <100327995+ManhHung01@users.noreply.github.com>
2022-05-29 23:11:32 +0700

    fixcart

--- Source Code/ShopOnline/src/java/dal/BlogDAO.java
+++ Source Code/ShopOnline/src/java/dal/BlogDAO.java
@@ -90,4 +90,52 @@ public class BlogDAO {
         }
         return lc;
     }
+    
 }
+/*public int countPage() {
+        try {
+            String sql = "SELECT COUNT(*) AS TOTAL FROM `blog`";
+            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                int countPage;
+                countPage = rs.getInt("TOTAL") / 2;
+                if (rs.getInt("TOTAL") % 2 != 0) {
+                    countPage++;
+                }
+                return countPage;
+            }
+        } catch (SQLException ex) {
+
+        } finally {
+            mysqlConnect.disconnect();
+        }
+        return 0;
+    }
+
+    public ArrayList<Blog> getBlogByPageNumber(int pageNumber) {
+        ArrayList<Blog> lc = new ArrayList<>();
+        try {
+            String sql = "SELECT blog_id , create_date, content, description, img "
+                    + "FROM(SELECT blog_id , create_date, content, description, img, "
+                    + "ROW_NUMBER() OVER (ORDER BY create_date DESC) AS Seq FROM  `blog`) as x "
+                    + "WHERE Seq BETWEEN ? AND ? ";
+            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+            statement.setInt(1, ((pageNumber - 1) * 2) + 1);
+            statement.setInt(2, (pageNumber * 2));
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Blog b = new Blog(rs.getInt("blog_id"),
+                        rs.getDate("create_date"),
+                        rs.getString("content"),
+                        rs.getString("description"),
+                        rs.getString("img"), pageNumber);
+                lc.add(b);
+            }
+        } catch (SQLException ex) {
+
+        } finally {
+            mysqlConnect.disconnect();
+        }
+        return lc;
+    }*/
