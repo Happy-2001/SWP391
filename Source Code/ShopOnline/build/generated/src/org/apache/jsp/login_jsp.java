7aeb792370c705cc739d6b3ebe9be3623992fd52
Hoang Viet <hoangnvliu@gmail.com>
2022-06-10 12:13:16 +0700

    update

---
+++ Source Code/ShopOnline/build/generated/src/org/apache/jsp/login_jsp.java
@@ -0,0 +1,183 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_if_test;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_if_test = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_if_test.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html lang=\"en\">\r\n");
+      out.write("<head>\r\n");
+      out.write("    <meta charset=\"UTF-8\">\r\n");
+      out.write("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n");
+      out.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n");
+      out.write("    <title>Login | Shop Online</title>\r\n");
+      out.write("    <!-- Site Icons -->\r\n");
+      out.write("    <link rel=\"shortcut icon\" href=\"images/favicon.ico\" type=\"image/x-icon\">\r\n");
+      out.write("    <link rel=\"apple-touch-icon\" href=\"images/apple-touch-icon.png\">\r\n");
+      out.write("    <link rel=\"stylesheet\" href=\"css/login.css\">\r\n");
+      out.write("    <link rel=\"stylesheet\" href=\"css/responsive.css\">\r\n");
+      out.write("    <script src=\"js/fontAwesome.js\"></script>\r\n");
+      out.write("\r\n");
+      out.write("    <!-- Bootstrap 5 -->\r\n");
+      out.write("    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js\"></script>\r\n");
+      out.write("    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor\" crossorigin=\"anonymous\">\r\n");
+      out.write("    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2\" crossorigin=\"anonymous\"></script>\r\n");
+      out.write("</head>\r\n");
+      out.write("<body>\r\n");
+      out.write("    <div class=\"Popup\" id=\"myModal\">\r\n");
+      out.write("        <div class=\"Popup-body\">\r\n");
+      out.write("            <div class=\"Pop-title\">\r\n");
+      out.write("                <h2>Login</h2>\r\n");
+      out.write("            </div>\r\n");
+      out.write("            <form action=\"login\" method=\"POST\">\r\n");
+      out.write("                <div class=\"Set-pop\">\r\n");
+      out.write("                    ");
+      if (_jspx_meth_c_if_0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("                    <div class=\"options\">\r\n");
+      out.write("                        <input id=\"user-name\" type=\"text\" name=\"username\" placeholder=\"Email or User Name\" required>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                    <div class=\"options\">\r\n");
+      out.write("                        <input id=\"user-pass\" type=\"password\" name=\"password\" placeholder=\"Password\" required>\r\n");
+      out.write("                        <i class=\"fa-regular fa-eye-slash showPass\"></i>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                    <div class=\"save-pass\">\r\n");
+      out.write("                        <label class=\"save-pass-container\">Remember password\r\n");
+      out.write("                            <input type=\"checkbox\" name=\"remember\">\r\n");
+      out.write("                            <span class=\"checkmark\"></span>\r\n");
+      out.write("                        </label>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                    <div class=\"options\">\r\n");
+      out.write("                        <button class=\"opt-btn\" type=\"submit\" name=\"created\">Go</button>\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                    <div class=\"options-divider\"></div>\r\n");
+      out.write("                    <div class=\"options\">\r\n");
+      out.write("                        <div class=\"auth-actions\">\r\n");
+      out.write("                            <a href=\"register\">Register a new account</a>\r\n");
+      out.write("                            <a href=\"forgot\">Forgot password?</a>\r\n");
+      out.write("                        </div>    \r\n");
+      out.write("                    </div>\r\n");
+      out.write("                </div>\r\n");
+      out.write("            </form>\r\n");
+      out.write("        </div>\r\n");
+      out.write("    </div>\r\n");
+      out.write("\r\n");
+      out.write("    <script>\r\n");
+      out.write("        var x = document.getElementById(\"user-pass\");\r\n");
+      out.write("        var show = document.querySelector(\".showPass\");\r\n");
+      out.write("        show.onclick = () => {     \r\n");
+      out.write("            if (x.type === \"password\") {\r\n");
+      out.write("                x.type = \"text\";\r\n");
+      out.write("                show.classList.replace(\"fa-eye-slash\",\"fa-eye\");\r\n");
+      out.write("            } else {\r\n");
+      out.write("                x.type = \"password\";\r\n");
+      out.write("                show.classList.replace(\"fa-eye\",\"fa-eye-slash\");\r\n");
+      out.write("            }\r\n");
+      out.write("        };\r\n");
+      out.write("        document.addEventListener('DOMContentLoaded', function () {\r\n");
+      out.write("            Validator({\r\n");
+      out.write("                form: '#form-2',\r\n");
+      out.write("                formGroupSelector: '.form-group',\r\n");
+      out.write("                errorSelector: '.form-message',\r\n");
+      out.write("                rules: [\r\n");
+      out.write("                    Validator.isEmail('#email'),\r\n");
+      out.write("                    Validator.minLength('#password', 6)\r\n");
+      out.write("                ]\r\n");
+      out.write("            });\r\n");
+      out.write("        });\r\n");
+      out.write("    </script>\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_c_if_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:if
+    org.apache.taglibs.standard.tag.rt.core.IfTag _jspx_th_c_if_0 = (org.apache.taglibs.standard.tag.rt.core.IfTag) _jspx_tagPool_c_if_test.get(org.apache.taglibs.standard.tag.rt.core.IfTag.class);
+    _jspx_th_c_if_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_if_0.setParent(null);
+    _jspx_th_c_if_0.setTest(((java.lang.Boolean) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${ms1 ne null}", java.lang.Boolean.class, (PageContext)_jspx_page_context, null)).booleanValue());
+    int _jspx_eval_c_if_0 = _jspx_th_c_if_0.doStartTag();
+    if (_jspx_eval_c_if_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\r\n");
+        out.write("                        <p class=\"alert alert-danger\">");
+        out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${ms1}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+        out.write("</p>\r\n");
+        out.write("                    ");
+        int evalDoAfterBody = _jspx_th_c_if_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_if_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+      return true;
+    }
+    _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
+    return false;
+  }
+}
