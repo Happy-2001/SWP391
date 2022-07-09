7a56fd9192f94f961bf9b970293fab1822b082c8
Đào Phúc Thạch <thachdp2808@gmail.com>
2022-07-09 23:49:38 +0700

    update SupplierDetail

--- Source Code/ShopOnline/src/java/controller/SupplierDetail.java
+++ Source Code/ShopOnline/src/java/controller/SupplierDetail.java
@@ -5,12 +5,14 @@
  */
 package controller;
 
+import dal.SuppliersDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Suppliers;
 
 /**
  *
@@ -30,7 +32,13 @@ public class SupplierDetail extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String id = request.getParameter("supID");
+        SuppliersDAO db = new SuppliersDAO();
+        Suppliers sup = db.getSupById(Integer.parseInt(id));
         
+        request.setAttribute("sup", sup);
+        
+        request.getRequestDispatcher("admin/SupplierDetail.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
