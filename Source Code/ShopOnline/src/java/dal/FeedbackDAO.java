de56786d4de09fcf755132caf9f119fc277a751c
Hoang Viet <hoangnvliu@gmail.com>
2022-07-13 11:45:08 +0700

    update docs

--- Source Code/ShopOnline/src/java/dal/FeedbackDAO.java
+++ Source Code/ShopOnline/src/java/dal/FeedbackDAO.java
@@ -24,48 +24,48 @@ public class FeedbackDAO extends DBConnect {
 
     DBConnect mysqlConnect = new DBConnect();
 
-    public List<MyFeedback> getFeedbackByDes(String seach) {
-        List<MyFeedback> statuses = new ArrayList<>();
-        String sql = "SELECT\n"
-                + "    user_account.full_name,\n"
-                + "    user_account.email,\n"
-                + "    user_account.phone,\n"
-                + "    products.product_name,\n"
-                + "    products.url,\n"
-                + "    feedback.star_rating,\n"
-                + "    feedback.description,\n"
-                + "    feedback.feedback_id,\n"
-                + "    feedback.User_Account_user_id\n"
-                + "FROM\n"
-                + "    feedback\n"
-                + "JOIN products products ON\n"
-                + "    feedback.products_product_id = products.product_id\n"
-                + "JOIN user_account ON feedback.User_Account_user_id = user_account.user_id WHERE feedback.description LIKE ? ";
-        try {
-            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
-            statement.setString(1, "%" + seach + "%");
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                MyFeedback s = new MyFeedback();
-                s.setFeedback_id(rs.getInt("feedback_id"));
-                s.setUserid(rs.getInt("User_Account_user_id"));
-                s.setFull_name(rs.getString("full_name"));
-                s.setUrl(rs.getString("url"));
-                s.setProduct_name(rs.getString("product_name"));
-                s.setStar_rating(rs.getInt("star_rating"));
-                s.setDescription(rs.getString("description"));
-                s.setUsemail(rs.getString("email"));
-                s.setPhone(rs.getInt("phone"));
-                statuses.add(s);
-            }
-            return statuses;
-        } catch (SQLException e) {
-            System.out.println(e);
-        } finally {
-            mysqlConnect.disconnect();
-        }
-        return null;
-    }
+//    public List<MyFeedback> getFeedbackByDes(String seach) {
+//        List<MyFeedback> statuses = new ArrayList<>();
+//        String sql = "SELECT\n"
+//                + "    user_account.full_name,\n"
+//                + "    user_account.email,\n"
+//                + "    user_account.phone,\n"
+//                + "    products.product_name,\n"
+//                + "    products.url,\n"
+//                + "    feedback.star_rating,\n"
+//                + "    feedback.description,\n"
+//                + "    feedback.feedback_id,\n"
+//                + "    feedback.User_Account_user_id\n"
+//                + "FROM\n"
+//                + "    feedback\n"
+//                + "JOIN products products ON\n"
+//                + "    feedback.products_product_id = products.product_id\n"
+//                + "JOIN user_account ON feedback.User_Account_user_id = user_account.user_id WHERE feedback.description LIKE ? ";
+//        try {
+//            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+//            statement.setString(1, "%" + seach + "%");
+//            ResultSet rs = statement.executeQuery();
+//            while (rs.next()) {
+//                MyFeedback s = new MyFeedback();
+//                s.setFeedback_id(rs.getInt("feedback_id"));
+//                s.setUserid(rs.getInt("User_Account_user_id"));
+//                s.setFull_name(rs.getString("full_name"));
+//                s.setUrl(rs.getString("url"));
+//                s.setProduct_name(rs.getString("product_name"));
+//                s.setStar_rating(rs.getInt("star_rating"));
+//                s.setDescription(rs.getString("description"));
+//                s.setUsemail(rs.getString("email"));
+//                s.setPhone(rs.getInt("phone"));
+//                statuses.add(s);
+//            }
+//            return statuses;
+//        } catch (SQLException e) {
+//            System.out.println(e);
+//        } finally {
+//            mysqlConnect.disconnect();
+//        }
+//        return null;
+//    }
 
     public int countPage() {
         try {
@@ -166,36 +166,36 @@ public class FeedbackDAO extends DBConnect {
     }
     
     //SELECT * FROM ( SELECT *, ROW_NUMBER() OVER (ORDER BY star_rating DESC) AS seq FROM `feedback`) as x WHERE seq BETWEEN 1 AND 2
-    public List<MyFeedback> getFbBySortRatePaging(int pageNumber, String order, String sort) {
-        List<MyFeedback> statuses = new ArrayList<>();
-        String sql = "SELECT * FROM (SELECT user_account.full_name, user_account.email, user_account.phone, products.product_name, products.url, feedback.star_rating,feedback.status,feedback.description, feedback.feedback_id, feedback.User_Account_user_id, ROW_NUMBER() OVER (ORDER BY " + order + " " + sort + ") AS seq FROM feedback JOIN products products ON feedback.products_product_id = products.product_id JOIN user_account ON feedback.User_Account_user_id = user_account.user_id) as x WHERE seq BETWEEN ? AND ?";
-        try {
-            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
-            statement.setInt(1, ((pageNumber - 1) * 2) + 1);
-            statement.setInt(2, (pageNumber * 2));
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                MyFeedback s = new MyFeedback();
-                s.setFeedback_id(rs.getInt("feedback_id"));
-                s.setUserid(rs.getInt("User_Account_user_id"));
-                s.setFull_name(rs.getString("full_name"));
-                s.setUrl(rs.getString("url"));
-                s.setProduct_name(rs.getString("product_name"));
-                s.setStar_rating(rs.getInt("star_rating"));
-                s.setDescription(rs.getString("description"));
-                s.setUsemail(rs.getString("email"));
-                s.setPhone(rs.getInt("phone"));
-                s.setStatus(rs.getInt("status"));
-                statuses.add(s);
-            }
-            return statuses;
-        } catch (SQLException ex) {
-            Logger.getLogger(FeedbackDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            mysqlConnect.disconnect();
-        }
-        return null;
-    }
+//    public List<MyFeedback> getFbBySortRatePaging(int pageNumber, String order, String sort) {
+//        List<MyFeedback> statuses = new ArrayList<>();
+//        String sql = "SELECT * FROM (SELECT user_account.full_name, user_account.email, user_account.phone, products.product_name, products.url, feedback.star_rating,feedback.status,feedback.description, feedback.feedback_id, feedback.User_Account_user_id, ROW_NUMBER() OVER (ORDER BY " + order + " " + sort + ") AS seq FROM feedback JOIN products products ON feedback.products_product_id = products.product_id JOIN user_account ON feedback.User_Account_user_id = user_account.user_id) as x WHERE seq BETWEEN ? AND ?";
+//        try {
+//            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+//            statement.setInt(1, ((pageNumber - 1) * 2) + 1);
+//            statement.setInt(2, (pageNumber * 2));
+//            ResultSet rs = statement.executeQuery();
+//            while (rs.next()) {
+//                MyFeedback s = new MyFeedback();
+//                s.setFeedback_id(rs.getInt("feedback_id"));
+//                s.setUserid(rs.getInt("User_Account_user_id"));
+//                s.setFull_name(rs.getString("full_name"));
+//                s.setUrl(rs.getString("url"));
+//                s.setProduct_name(rs.getString("product_name"));
+//                s.setStar_rating(rs.getInt("star_rating"));
+//                s.setDescription(rs.getString("description"));
+//                s.setUsemail(rs.getString("email"));
+//                s.setPhone(rs.getInt("phone"));
+//                s.setStatus(rs.getInt("status"));
+//                statuses.add(s);
+//            }
+//            return statuses;
+//        } catch (SQLException ex) {
+//            Logger.getLogger(FeedbackDAO.class.getName()).log(Level.SEVERE, null, ex);
+//        } finally {
+//            mysqlConnect.disconnect();
+//        }
+//        return null;
+//    }
 
     public void addFB(String des, int uid, int pid, int rate) {
         String sql = "INSERT INTO `feedback` (`description`, `User_Account_user_id`, `products_product_id`, `star_rating`,`note`, `status` ) VALUES (?, ?, ?, ?, ?, ?)";
@@ -216,47 +216,47 @@ public class FeedbackDAO extends DBConnect {
         }
     }
 
-    public List<MyFeedback> getCourseByPageNumber(int pageNumber) {
-        List<MyFeedback> statuses = new ArrayList<>();
-        String sql = "SELECT\n"
-                + "    user_account.full_name,\n"
-                + "    user_account.email,\n"
-                + "    user_account.phone,\n"
-                + "    products.product_name,\n"
-                + "    products.url,\n"
-                + "    feedback.star_rating,\n"
-                + "    feedback.description,\n"
-                + "    feedback.feedback_id,\n"
-                + "    feedback.User_Account_user_id\n"
-                + "FROM\n"
-                + "    feedback\n"
-                + "JOIN products products ON\n"
-                + "    feedback.products_product_id = products.product_id\n"
-                + "JOIN user_account ON feedback.User_Account_user_id = user_account.user_id    ;";
-        try {
-            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                MyFeedback s = new MyFeedback();
-                s.setFeedback_id(rs.getInt("feedback_id"));
-                s.setUserid(rs.getInt("User_Account_user_id"));
-                s.setFull_name(rs.getString("full_name"));
-                s.setUrl(rs.getString("url"));
-                s.setProduct_name(rs.getString("product_name"));
-                s.setStar_rating(rs.getInt("star_rating"));
-                s.setDescription(rs.getString("description"));
-                s.setUsemail(rs.getString("email"));
-                s.setPhone(rs.getInt("phone"));
-                statuses.add(s);
-            }
-            return statuses;
-        } catch (SQLException ex) {
-            Logger.getLogger(FeedbackDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            mysqlConnect.disconnect();
-        }
-        return null;
-    }
+//    public List<MyFeedback> getCourseByPageNumber(int pageNumber) {
+//        List<MyFeedback> statuses = new ArrayList<>();
+//        String sql = "SELECT\n"
+//                + "    user_account.full_name,\n"
+//                + "    user_account.email,\n"
+//                + "    user_account.phone,\n"
+//                + "    products.product_name,\n"
+//                + "    products.url,\n"
+//                + "    feedback.star_rating,\n"
+//                + "    feedback.description,\n"
+//                + "    feedback.feedback_id,\n"
+//                + "    feedback.User_Account_user_id\n"
+//                + "FROM\n"
+//                + "    feedback\n"
+//                + "JOIN products products ON\n"
+//                + "    feedback.products_product_id = products.product_id\n"
+//                + "JOIN user_account ON feedback.User_Account_user_id = user_account.user_id    ;";
+//        try {
+//            PreparedStatement statement = mysqlConnect.connect().prepareStatement(sql);
+//            ResultSet rs = statement.executeQuery();
+//            while (rs.next()) {
+//                MyFeedback s = new MyFeedback();
+//                s.setFeedback_id(rs.getInt("feedback_id"));
+//                s.setUserid(rs.getInt("User_Account_user_id"));
+//                s.setFull_name(rs.getString("full_name"));
+//                s.setUrl(rs.getString("url"));
+//                s.setProduct_name(rs.getString("product_name"));
+//                s.setStar_rating(rs.getInt("star_rating"));
+//                s.setDescription(rs.getString("description"));
+//                s.setUsemail(rs.getString("email"));
+//                s.setPhone(rs.getInt("phone"));
+//                statuses.add(s);
+//            }
+//            return statuses;
+//        } catch (SQLException ex) {
+//            Logger.getLogger(FeedbackDAO.class.getName()).log(Level.SEVERE, null, ex);
+//        } finally {
+//            mysqlConnect.disconnect();
+//        }
+//        return null;
+//    }
 
     public MyFeedback getFeedbackById(int id) {
         String sql = "SELECT fb.feedback_id, fb.image1, fb.image2, fb.description,\n" +
